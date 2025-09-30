10d87b4462036c7ac0e01ffb30f3a38d32dca715
Tobias Weber <weber.tobias.md@gmail.com>
2025-09-30 22:44:24 +0200

    changes to ci

--- python/setup.py
+++ python/setup.py
@@ -239,7 +239,7 @@ if __name__ == "__main__":
         ext_modules=ext_modules,
         cmdclass=cmdclass,
         zip_safe=False,
-        python_requires=">=3.7",
+        python_requires=">=3.12",
         classifiers=[
             "Development Status :: 4 - Beta",
             "Intended Audience :: Developers",
